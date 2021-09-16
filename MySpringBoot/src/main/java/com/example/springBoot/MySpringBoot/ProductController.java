a6be846c76d41b0913c13820ca0d341e2e00f537
Pranjal-1729 <49397202+Pranjal-1729@users.noreply.github.com>
2021-09-16 11:05:39 +0530

    Add files via upload

---
+++ MySpringBoot/src/main/java/com/example/springBoot/MySpringBoot/ProductController.java
@@ -0,0 +1,5 @@
+package com.example.springBoot.MySpringBoot;
+
+public class ProductController {
+
+}
---
+++ MySpringBoot/src/test/java/com/example/springBoot/MySpringBoot/MySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBoot.MySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
